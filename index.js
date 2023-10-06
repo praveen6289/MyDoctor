2f99c1057275440127117c7861e8d12369941b9a
praveenkumar.kuthadi <praveenkumar.kuthadi@scriptbees.com>
2023-10-06 23:26:32 +0530

    my changes are updated

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#foo").click(function(){
+        $("div").toggleClass("box2");
+    });
+
+    $("#welcome").click(function(){
+        alert("Hi Welcome to my Application");
+    })
+
+
+});
+  
